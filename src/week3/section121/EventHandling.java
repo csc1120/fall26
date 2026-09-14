84482a38aa009f2b0486c2ac9d31f32078cbef00
sean <jones@msoe.edu>
2026-09-14 10:39:05 -0600

    Lecture 8 121

--- src/week3/section121/EventHandling.java
+++ src/week3/section121/EventHandling.java
@@ -8,24 +8,73 @@
 package week3.section121;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.effect.BoxBlur;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 /**
  * Example program showing various ways to implement EventHandlers.
  */
 public class EventHandling extends Application {
+    private Button button;
+
     static void main() {
         launch();
     }
 
     @Override
     public void start(Stage stage) {
-        final int dimensions = 100;
-        StackPane root = new StackPane(new Button("Hello"));
+        final int dimensions = 200;
+        final int width = 70;
+        final int height = 60;
+        final int fontSize = 30;
+
+        button = new Button("Hello");
+        button.setMinWidth(width);
+        button.setMinHeight(height);
+        button.setFont(new Font(fontSize));
+        // anonymous inner class
+//        button.setOnAction(new EventHandler<ActionEvent>() {
+//            @Override
+//            public void handle(ActionEvent actionEvent) {
+//                if(button.getEffect() == null) {
+//                    button.setEffect(new BoxBlur());
+//                } else {
+//                    button.setEffect(null);
+//                }
+//            }
+//        });
+        // lambda expression
+        button.setOnAction(_ -> blurButton());
+        // method reference
+//        button.setOnAction(this::blurButton);
+        StackPane root = new StackPane(button);
         stage.setScene(new Scene(root, dimensions, dimensions));
         stage.show();
+    }
+
+    private void blurButton() {
+        if(button.getEffect() == null) {
+            button.setEffect(new BoxBlur());
+        } else {
+            button.setEffect(null);
+        }
+    }
+
+    private class ButtonHandler implements EventHandler<ActionEvent> {
+        @Override
+        public void handle(ActionEvent actionEvent) {
+//            Button b = (Button) actionEvent.getSource();
+            if(button.getEffect() == null) {
+                button.setEffect(new BoxBlur());
+            } else {
+                button.setEffect(null);
+            }
+        }
     }
 }
